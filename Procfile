db92922fab7c3567618a4cdad299be616e908942
fattoretti <samuele.fattoretti@gmail.com>
2020-05-28 11:03:47 +0200

    e

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mysiteFattoretti.wsgi --log-file - 
--- mysiteFattoretti/settings.py
+++ mysiteFattoretti/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z$#j!mtge37&#wp7ye#y8#m(dzs-*zdzp3#i6oc5n20l0v8&_y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'mysiteFattoretti.urls'
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
